19ada8cc97af9d9a51537e2f163b54db18b6b93b
Alejandro Nanez <alejonanez@gmail.com>
2018-09-27 04:51:20 -0500

    seeding data

--- lib/rumbl/multimedia/multimedia.ex
+++ lib/rumbl/multimedia/multimedia.ex
@@ -6,88 +6,28 @@ defmodule Rumbl.Multimedia do
   import Ecto.Query, warn: false
   alias Rumbl.Repo
   alias Rumbl.Accounts
+  alias Rumbl.Multimedia.Category
 
   alias Rumbl.Multimedia.Video
 
-  @doc """
-  Returns the list of videos.
-
-  ## Examples
-
-      iex> list_videos()
-      [%Video{}, ...]
-
-  """
   def list_videos do
     Video
     |> Repo.all(Video)
     |> preload_user
   end
 
-  @doc """
-  Gets a single video.
+  def create_category(name) do
+    Repo.get_by(Category, name: name) || Repo.insert!(%Category{name: name})
+  end
 
-  Raises `Ecto.NoResultsError` if the Video does not exist.
-
-  ## Examples
-
-      iex> get_video!(123)
-      %Video{}
-
-      iex> get_video!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_video!(id), do: preload_user(Repo.get!(Video, id))
 
-  @doc """
-  Creates a video.
-
-  ## Examples
-
-      iex> create_video(%{field: value})
-      {:ok, %Video{}}
-
-      iex> create_video(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  # def create_video(attrs \\ %{}) do
-  #   %Video{}
-  #   |> Video.changeset(attrs)
-  #   |> Repo.insert()
-  # end
-
-  @doc """
-  Updates a video.
-
-  ## Examples
-
-      iex> update_video(video, %{field: new_value})
-      {:ok, %Video{}}
-
-      iex> update_video(video, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_video(%Video{} = video, attrs) do
     video
     |> Video.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a Video.
-
-  ## Examples
-
-      iex> delete_video(video)
-      {:ok, %Video{}}
-
-      iex> delete_video(video)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_video(%Video{} = video) do
     Repo.delete(video)
   end
