19485e1583639f517ddf8a0c50c151ec77d6b63c
Alejandro Nanez <alejonanez@gmail.com>
2018-09-30 18:04:22 -0500

    Add testing

--- test/rumbl/accounts/accounts_test.exs
+++ test/rumbl/accounts/accounts_test.exs
@@ -2,66 +2,54 @@ defmodule Rumbl.AccountsTest do
   use Rumbl.DataCase
 
   alias Rumbl.Accounts
+  alias Rumbl.Accounts.User
 
-  describe "credentials" do
-    alias Rumbl.Accounts.Credential
+  describe "register_user/2" do
+    @valid_attrs %{
+      name: "User",
+      username: "eva",
+      credential: %{email: "eva@test", password: "secret"}
+    }
+    @invalid_attrs %{}
 
-    @valid_attrs %{email: "some email", password_hash: "some password_hash"}
-    @update_attrs %{email: "some updated email", password_hash: "some updated password_hash"}
-    @invalid_attrs %{email: nil, password_hash: nil}
-
-    def credential_fixture(attrs \\ %{}) do
-      {:ok, credential} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Accounts.create_credential()
-
-      credential
+    test "widh valid data insert user" do
+      assert {:ok, %User{id: id} = user} = Accounts.register_user(@valid_attrs)
+      assert user.name == "User"
+      assert user.username == "eva"
+      assert user.credential.email == "eva@test"
+      assert [%User{id: ^id}] = Accounts.list_users()
     end
 
-    test "list_credentials/0 returns all credentials" do
-      credential = credential_fixture()
-      assert Accounts.list_credentials() == [credential]
+    test "with invalid data does not insert user" do
+      assert {:error, _changeset} = Accounts.register_user(@invalid_attrs)
+      assert Accounts.list_users() == []
     end
 
-    test "get_credential!/1 returns the credential with given id" do
-      credential = credential_fixture()
-      assert Accounts.get_credential!(credential.id) == credential
+    test "enforces unique usernames" do
+      assert {:ok, %User{id: id}} = Accounts.register_user(@valid_attrs)
+      assert {:error, changeset} = Accounts.register_user(@valid_attrs)
+
+      assert %{username: ["has already been taken"]} = errors_on(changeset)
+
+      assert [%User{id: ^id}] = Accounts.list_users()
     end
 
-    test "create_credential/1 with valid data creates a credential" do
-      assert {:ok, %Credential{} = credential} = Accounts.create_credential(@valid_attrs)
-      assert credential.email == "some email"
-      assert credential.password_hash == "some password_hash"
+    test "does not accept long usernames" do
+      attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
+      {:error, changeset} = Accounts.register_user(attrs)
+
+      assert %{username: ["should be at most 20 character(s)"]} = errors_on(changeset)
+      assert Accounts.list_users() == []
     end
 
-    test "create_credential/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Accounts.create_credential(@invalid_attrs)
-    end
+    test "requires password to be at least 6 chars long" do
+      attrs = put_in(@valid_attrs, [:credential, :password], "12345")
+      {:error, changeset} = Accounts.register_user(attrs)
 
-    test "update_credential/2 with valid data updates the credential" do
-      credential = credential_fixture()
-      assert {:ok, %Credential{} = credential} = Accounts.update_credential(credential, @update_attrs)
-      
-      assert credential.email == "some updated email"
-      assert credential.password_hash == "some updated password_hash"
-    end
+      assert %{password: ["should be at least 6 character(s)"]} =
+               errors_on(changeset)[:credential]
 
-    test "update_credential/2 with invalid data returns error changeset" do
-      credential = credential_fixture()
-      assert {:error, %Ecto.Changeset{}} = Accounts.update_credential(credential, @invalid_attrs)
-      assert credential == Accounts.get_credential!(credential.id)
-    end
-
-    test "delete_credential/1 deletes the credential" do
-      credential = credential_fixture()
-      assert {:ok, %Credential{}} = Accounts.delete_credential(credential)
-      assert_raise Ecto.NoResultsError, fn -> Accounts.get_credential!(credential.id) end
-    end
-
-    test "change_credential/1 returns a credential changeset" do
-      credential = credential_fixture()
-      assert %Ecto.Changeset{} = Accounts.change_credential(credential)
+      assert Accounts.list_users() == []
     end
   end
 end
