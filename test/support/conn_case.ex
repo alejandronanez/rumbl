5a8deca746658b8bbef41f7e3f9815475ae686eb
Alejandro Nanez <alejonanez@gmail.com>
2018-10-06 14:54:23 -0500

    Adding tests to the video_controller

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -19,6 +19,9 @@ defmodule RumblWeb.ConnCase do
     quote do
       # Import conveniences for testing with connections
       use Phoenix.ConnTest
+
+      import Rumbl.TestHelpers
+
       alias RumblWeb.Router.Helpers, as: Routes
 
       # The default endpoint for testing
@@ -26,13 +29,13 @@ defmodule RumblWeb.ConnCase do
     end
   end
 
-
   setup tags do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Rumbl.Repo)
+
     unless tags[:async] do
       Ecto.Adapters.SQL.Sandbox.mode(Rumbl.Repo, {:shared, self()})
     end
+
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
-
 end
